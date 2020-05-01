9428374c2bfd1122ec3f6b952b638dcb8930b6d2
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-05-01 10:30:49 +0800

    hw0501

--- Class3hw/Assets/Button.cs
+++ Class3hw/Assets/Button.cs
@@ -21,27 +21,27 @@ public class Button: MonoBehaviour
     public void Btatk()
     {
         //輸出結果.文字內容 =
-        result.text = "史萊姆受到蝙蝠" + bat.atk + "點的攻擊" +
-            "\n" + "史萊姆血量：" + (slime.hp -= bat.atk);
+        result.text = "<color=#008000ff>史萊姆</color>"+ "受到" + "<color=#000000ff>蝙蝠</color>" + bat.atk + "點的攻擊" +
+            "\n" + "<color=#008000ff>史萊姆</color>" + "血量：" + (slime.hp -= bat.atk);
     }
 
     public void Btheal()
     {
-        result.text = "蝙蝠受到" + bat.heal + "點的治癒" +
-            "\n" + "蝙蝠血量：" + (bat.hp += bat.heal);
+        result.text = "<color=#000000ff>蝙蝠</color>" +"受到" + bat.heal + "點的治癒" +
+            "\n" + "<color=#000000ff>蝙蝠</color>" +"血量：" + (bat.hp += bat.heal);
     }
 
     public void Slatk()
     {
-        result.text = "蝙蝠受到史萊姆" + slime.atk + "點的攻擊" +
-            "\n" + "蝙蝠血量：" + (bat.hp -= slime.atk);
+        result.text = "<color=#000000ff>蝙蝠</color>" + "受到" + "<color=#008000ff>史萊姆</color>" + slime.atk + "點的攻擊" +
+            "\n" + "<color=#000000ff>蝙蝠</color>"  + "血量：" + (bat.hp -= slime.atk);
     }
 
 
     public void Slheal()
     {
-        result.text = "史萊姆受到" + slime.heal + "點的治癒" +
-            "\n" + "史萊姆血量：" + (slime.hp += slime.heal);
+        result.text = "<color=#008000ff>史萊姆</color>"+ "受到" + slime.heal + "點的治癒" +
+            "\n" + "<color=#008000ff>史萊姆</color>"+ "血量：" + (slime.hp += slime.heal);
     }
 
 
